f2cb8d52796ddb33aaa7cee22dc3693c09448153
なかよう <72610232+NakaYou@users.noreply.github.com>
2021-01-20 23:31:09 +0900

    +Achievements

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,6 +3,15 @@ import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar'
 
+function Achievements(){
+  return(
+    <div>
+      <h1>Achievements</h1>
+      <p>nothing</p>
+    </div>
+  )
+}
+
 export default function Home() {
   return (
     <div className={styles.page}>
@@ -23,6 +32,7 @@ export default function Home() {
       <p>どうもこんにちは，なかようといいます．</p>
       <p>あっという間に二年生になってしまいますが僕は入学当初から何も変わってない気がしています．</p>
       <p>のんびりしていられないですね．</p>
+      <Achievements />
     </div>
   )
 }
