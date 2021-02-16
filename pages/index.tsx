e83524802583118ec10fd6c0d5a20503527a675d
なかよう <72610232+NakaYou@users.noreply.github.com>
2021-02-16 21:39:13 +0900

    fix

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar'
-import react, { useState } from 'react'
+import { useState } from 'react'
 
 function Achievements(){
   return(
