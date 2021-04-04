fb21787c8be751bed17cb650cf96377466538736
なかよう <72610232+NakaYou@users.noreply.github.com>
2021-04-04 21:43:48 +0900

    -disqus

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -2,7 +2,6 @@ import styles from '../../styles/Home.module.css'
 import Navbar from '../../components/Navbar'
 import Head from 'next/head'
 import { getAllPostIds, getPostData } from '../../lib/posts'
-import DisqusComments from '../../components/disqusComments'
 
 export async function getStaticProps({ params }) {
   const postData = await getPostData(params.id)
@@ -25,7 +24,6 @@ export default function Post({ postData }) {
       {postData.date}
       <br />
       <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
-      <DisqusComments post="blog.tsx" />
       <Navbar />
     </div>
 
