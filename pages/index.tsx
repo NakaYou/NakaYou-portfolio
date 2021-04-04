1cee27b6fb0fb542cb7c39a10e2471f941d3948b
なかよう <72610232+NakaYou@users.noreply.github.com>
2021-04-04 21:30:00 +0900

    color change

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,17 +4,32 @@ import styles from '../styles/Home.module.css'
 import Navbar from '../components/Navbar'
 import { useState } from 'react'
 
-function Achievements(){
-  return(
+function Achievements() {
+  return (
     <div>
-      <h1>Achievements</h1>
-      <p>nothing</p>
+      <h1>Products</h1>
+      <div className={styles.NakaYou}>
+        <a href={"https://hit-and-blow-delta.vercel.app"}>
+          <Image src={"/images/HB.png"}
+            width={200} height={200} className={styles.img} />
+          <h1>Hit&Blow</h1>
+          <p>reactでhit&blowを作りました</p>
+        </a>
+      </div>
+      <div className={styles.NakaYou}>
+        <a href={"https://ten-second-challenge.vercel.app"}>
+          <Image src={"/images/Sec10.png"}
+            width={200} height={200} className={styles.img}/>
+            <h1>10sec Challenge</h1>
+            <p>reactで10秒連打するゲームを作りました</p>
+        </a>
+      </div>
     </div>
   )
 }
 
 export default function Home() {
-  const[nakayou,setimage] = useState(true);
+  const [nakayou, setimage] = useState(true);
   return (
     <div className={styles.page}>
       <Head>
@@ -24,9 +39,9 @@ export default function Home() {
       <Navbar />
       <div className={styles.NakaYou}>
         <Image
-          src={nakayou?"/images/NakaYou.png":"/images/NakaYou2.png"}
-          width={200}
-          height={200} onClick={() => {setimage(!nakayou)}} />
+          src={nakayou ? "/images/NakaYou.png" : "/images/NakaYou2.png"}
+          width={200} className={styles.img}
+          height={200} onClick={() => { setimage(!nakayou) }} />
       </div>
 
       <h1>NakaYou</h1>
@@ -34,7 +49,7 @@ export default function Home() {
       <p>あっという間に二年生になってしまいますが僕は入学当初から何も変わってない気がしています．</p>
       <p>のんびりしていられないですね．</p>
       <h1>
-      <a href="https://twitter.com/compose/tweet?text=なかようのサイトを見ました！ nakayou.vercel.app">Twitterでシェア</a>
+        <a href="https://twitter.com/compose/tweet?text=なかようのサイトを見ました！ nakayou.vercel.app">Twitterでシェア</a>
       </h1>
       <Achievements />
     </div>
